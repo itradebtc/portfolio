02460055bd8d6c2ee6bb9856a428d5b6ebb734a0
USER <you@example.com>
2023-08-17 12:52:41 +0100

    portfolio

--- src/app/portfolio/layout.jsx
+++ src/app/portfolio/layout.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Montserrat } from 'next/font/google'
+
+const mont = Montserrat({ subsets:['latin'], 
+weight:['100', '200', '300', '400', '500', '600', '700', '800', '900']})
 
 export const metadata = {
     title: 'Portfolio',
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -12,16 +12,16 @@ export default function page(){
         <button className='border border-black py-2 px-4'><a href='../public/mycv.pdf' download="mycv.pdf">Download CV</a></button>
       </div> */}
         <div className='flex flex-col md:gap-5 text-center justify-center items-center w-auto md:justify-center md:text-center font-bold m-2 md:m-0'>
-            <h1 className='uppercase text-2xl md:text-7xl'>coming soon</h1>
-            <h2>I&#39;m</h2>
-            <h3 className='uppercase text-lg md:text-4xl text-green-800'>
+            <h1 className='uppercase text-2xl md:text-7xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent animate-bounce'>coming soon</h1>
+            <h2 className='uppercase text-lg md:text-4xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent'>I&#39;m a</h2>
+            <h3 className='uppercase text-lg md:text-4xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent'>
             <Typewriter 
                  options={{
                     strings: [
                       "Web developer",
-                      "Android developer",
                       "Native React Developer",
-                      "React Js Developer"
+                      "React.Js Developer",
+                      "Next.Js Developer"
                     ],
                     autoStart: true,
                     loop: true,
@@ -31,7 +31,7 @@ export default function page(){
             </h3>
         </div>
         <div className='w-auto p-4 md:p-7'>
-          <div className='grid grid-cols-[200px,200px] grid-rows-[100px, 100px] pt-6 md:gap-8 md:flex md:justify-around items-center md:py-16 md:px-12 md:text-xl'>
+          <div className='grid grid-cols-[200px,200px] grid-rows-[100px, 100px] pt-6 md:gap-8 md:flex md:justify-around items-center md:py-16 md:px-12 md:text-xl md:font-black'>
               <AnimatePresence>
                 <motion.div
                 initial={{ opacity: 0, y: 500 }}
@@ -39,10 +39,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .3, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     html
-                    <SiHtml5 size={100} />
                     </p>
+                    <SiHtml5 size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence>    
               <AnimatePresence>
@@ -52,10 +52,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .4, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     css
-                    <SiCss3 size={100} />
                     </p>
+                    <SiCss3 size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence>    
               <AnimatePresence>
@@ -65,10 +65,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .5, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     javascript
-                    <SiJavascript size={100} />
                     </p>
+                    <SiJavascript size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence>    
               <AnimatePresence>
@@ -78,10 +78,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .6, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     tailwind
-                    <SiTailwindcss size={100} />
                     </p>
+                    <SiTailwindcss size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence>    
               <AnimatePresence>
@@ -91,10 +91,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .7, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     react
-                    <SiReact size={100} />
                     </p>
+                    <SiReact size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence>    
               <AnimatePresence>
@@ -104,10 +104,10 @@ export default function page(){
                 exist={{ opacity: 0, y: 0 }}
                 transition={{ delay: .8, duration: 1, ease: [0.22, 1, 0.36, 1]}}
                 >
-                    <p className='uppercase md:text-center md:flex md:flex-col'>
+                    <p className='uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent md:text-center md:flex md:flex-col'>
                     next.js
-                    <TbBrandNextjs size={100} />
                     </p>
+                    <TbBrandNextjs size={100} className='text-green-800' />
                 </motion.div>
               </AnimatePresence> 
           </div>
