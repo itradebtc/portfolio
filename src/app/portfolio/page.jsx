4f0bb5fbdd8b08587917e9dffa9447551e9d0b5d
USER <you@example.com>
2023-08-17 13:58:08 +0100

    portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -12,7 +12,7 @@ export default function page(){
         <button className='border border-black py-2 px-4'><a href='../public/mycv.pdf' download="mycv.pdf">Download CV</a></button>
       </div> */}
         <div className='flex flex-col md:gap-5 text-center justify-center items-center w-auto md:justify-center md:text-center font-bold m-2 md:m-0'>
-            <h1 className='uppercase text-2xl md:text-7xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent animate-bounce'>coming soon</h1>
+            <h1 className='uppercase text-2xl md:text-7xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent'>coming soon</h1>
             <h2 className='uppercase text-lg md:text-4xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent'>I&#39;m a</h2>
             <h3 className='uppercase text-lg md:text-4xl bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent'>
             <Typewriter 
@@ -31,7 +31,19 @@ export default function page(){
             </h3>
         </div>
         <div className='w-auto p-4 md:p-7'>
-          <div className='grid grid-cols-[200px,200px] grid-rows-[100px, 100px] w-auto pl-10 pt-6 md:gap-8 md:flex md:justify-around items-center md:py-16 md:px-12 md:text-xl md:font-black'>
+          <section className='text-3xl uppercase bg-gradient-to-bl from-neutral-600 via-rose-600 to-indigo-500 bg-clip-text text-transparent pt-7'>
+              <AnimatePresence>
+                <motion.div
+                initial={{ opacity: 0, y: 15 }}
+                animate={{ opacity: 1, y: 0 }}
+                exist={{ opacity: 0, y: 0 }}
+                transition={{ delay: 1, duration: 1, ease: [0.22, 1, 0.36, 1]}}
+                >
+                  <h3>My Skills</h3>
+                </motion.div>
+              </AnimatePresence>
+          </section>
+          <div className='grid grid-cols-[200px,200px] grid-rows-[100px, 100px] w-auto pl-10 pt-3 md:gap-8 md:flex md:justify-around items-center md:py-16 md:px-12 md:text-xl md:font-black'>
               <AnimatePresence>
                 <motion.div
                 initial={{ opacity: 0, y: 500 }}
